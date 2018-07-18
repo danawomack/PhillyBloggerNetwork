9842f1e3c52e1e160f2800242ad5ec88d62b4288
Dana Womack <dananwomack@mgail.com>
2018-07-17 21:50:45 -0400

    deployment changes

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-
+//below added for heroku deployment
+const path = require("path")
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
+//below added for heroku deployment
+// app.use(express.static(path.join(__dirname, "client", "build")))
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(require("express-session")({
@@ -33,6 +37,9 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 // // Add routes, both API and view
 const routes = require("./routes/auth-api");
@@ -41,12 +48,32 @@ const post_routes = require("./routes/posts-api");
 app.use(routes);
 app.use(post_routes);
 
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function (req, res) {
+  //use this in development
+  //res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  //change to this in production
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/phillyblogdb");
+
+//below added for heroku deployment
+// app.get("*", (req, res) => {  
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
+
+
 
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
   console.log(`🌎  ==> http://localhost:${PORT}!`);
 });
+
+
+//serving public static files express serving public files
